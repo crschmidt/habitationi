d036b2f21567f4723c38b122f7d16b94b2eeb896
Christopher Schmidt <crschmidt@crschmidt.net>
2024-11-09 01:00:35 +0000

    Add nonconf report.

---
+++ shared/classes.py
@@ -0,0 +1,103 @@
+class_lookup = {
+        'SNGL-FAM-RES': "Single family home",
+        'TWO-FAM-RES': "Two-family home",
+        'CONDO-BLDG': "Condo building",
+        '>8-UNIT-APT': 'Large apartment building',
+        'THREE-FM-RES': 'Multi-family home',
+        '4-8-UNIT-APT': 'Apartment building',
+        'MULTIUSE-RES': 'Multi-use residential',
+        'SINGLE FAM W/AUXILIARY APT': 'Single family home+apartment',
+        'RETAIL-STORE': 'Retail store',
+        'GEN-OFFICE': 'Office space',
+        'Vacant City': 'Vacant city property',
+        'MULTIPLE-RES': 'Multi-family residence',
+        'MULTIUSE-COM': 'Multi-use commercial',
+        'Church': 'Church',
+        'RETAIL-OFFIC': 'Retail space',
+        'PARKING-LOT': 'Parking lot',
+        'RES-UDV-LAND': 'Empty lot',
+        'RES-DEV-LAND': 'Empty lot',
+        'WAREHOUSE': 'Warehouse',
+        'EATING-ESTBL': 'Eating establishment',
+        'HIGH-TECH': 'High tech',
+        'INV-OFFICE': 'Office space',
+        'DCR- State Parks and Rec': 'State park',
+        'Vacant (Private Ed)': 'Vacant lot',
+        'Vacnt Transport Authorit': 'Vacant MBTA',
+        'Improved City': 'City lot',
+        'COM-DEV-LAND': 'Developed lot',
+        'Private College, University': 'Private College',
+        'RES-UDV-PARK LND': 'Private park',
+        'MULT-RES-4-8-APT': 'Apartment building',
+#        RES-&-DEV-FC
+#        Rectory, Parsonage
+        'GAS-STATION': 'Gas Station',
+        'PUB UTIL REG': 'Utility',
+        'AUTO-REPAIR': 'Auto Repair',
+        'CNDO LUX': 'Luxury Condo',
+        'HOTEL': 'Hotel',
+        'BOARDING-HSE': 'Boarding house',
+        'COM-UDV-LAND': 'Empty lot',
+#        Improved Local Edu
+#        CONDOMINIUM
+#        FRAT-ORGANIZ
+#        MANUFACTURNG
+#121 Corporation
+#COM-PDV-LAND
+#CLEAN-MANUF
+#Vacant Local Education
+        'BANK': 'Bank',
+#Improved Public Safety
+#MULT-RES-2FAM
+'PARKING-GAR': 'Parking Garage',
+#CNDO-RES-PKG
+'MEDICAL-OFFC': 'Medical Office',
+#MULT-RES-1FAM
+'SH-CNTR/MALL': 'Shopping Center',
+#CHILD-CARE
+#RES-LAND-IMP
+#US Government
+#FRAT-SORORTY
+#MULT-RES->8 APT
+#MULT-RES-3FAM
+#Other
+#Private Elementary Education
+#RES LND-IMP UNDEV
+#RES-PDV-LAND
+#ASSISTED-LIV
+#DORM-RS-HALL
+#Private Secondary Education
+#SUPERMARKET
+#Transportation Authority
+#Vacant, Tax Title
+#AUTO-SALES
+#INN-RESORT
+#Imprvd County Admin
+#MXD RETAIL-STORE
+#Other- Scientific
+#RES LND-IMP PT DEV
+#TELE-EXCH-STA
+#Utility Authority
+#ELECT-PLANT
+#Hospitals
+#IND-DEV-LAND
+#MULTIUSE-IND
+#MXD 4-8-UNIT-APT
+#THEATRE
+#Vacant Utility Authority
+#AUTO-SUPPLY
+#BILLBOARD
+#Cemeteries
+#ELEC GEN PLANT
+#GAS-CONTROL
+#IND-UDV-LAND
+#INDUST-CONDO
+#MXD GEN-OFFICE
+#NURSING-HOME
+#Other Charitable
+#Other Educational
+#Other Open Space
+#TENNIS-CLUB
+#Vacant Housing Authority
+}
+
